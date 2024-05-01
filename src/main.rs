40b8b2ba15229c66d3ebcbeb2a1cb36e69b2ebde
nartan <bozec.tanguy@gmail.com>
2024-05-02 00:16:59 +0200

    finished pest file generation for non-regex declarations

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,68 @@ use pest_derive::Parser;
 
 use std::fs;
 
+fn concatable(token_rule: Rule) -> bool {
+    match token_rule {
+        Rule::non_terminal | Rule::token | Rule::name => {
+            return true;
+        }
+        _ => {return false;}
+    }
+}
+
+
+fn base_to_str(pairs: Pairs<Rule>) -> String {
+    let mut res:Vec<String> = Vec::new();
+    let mut pairs = pairs.into_iter();
+    while let Some(pair) = pairs.next() {
+        let pair_rule = pair.as_rule();
+        match pair_rule {
+            Rule::non_terminal => {
+                res.push(pair.into_inner().next().unwrap().as_str().to_string());
+            }
+            Rule::pattern => {
+                res.push(base_to_str(pair.into_inner()));
+            }
+            Rule::token => {
+                res.push("\"".to_string() + pair.as_str() + "\"");
+            }
+            Rule::or | Rule::star | Rule::name => {
+                res.push(pair.as_str().to_string());
+            }
+            _ => {}
+        }
+        if concatable(pair_rule) && pairs.peek().is_some_and(|r| concatable(r.as_rule())) {
+            res.push("~".to_string());
+        }
+    }
+    return res.join(" ");
+}
+
 fn generate_pest(pairs : Pairs<Rule>) -> &str {
-    let mut result = "";
     for pair in pairs.clone() {
         match pair.as_rule() {
             Rule::syntax_rule | Rule::regex_rule => {
-                let rule = pair.into_inner();
-                println!("{} = {{}}\n", rule);
+                let mut rule = pair.into_inner();
+
+                let non_terminal = rule.next().unwrap();
+                let name = base_to_str(non_terminal.into_inner());
+
+                let expansion = rule.skip(1).next().unwrap().into_inner();
+
+
+                print!("\n{} = ", name);
+                print!("{{ {} }}\n", base_to_str(expansion));
             }
-            _ => {
+            Rule::file | Rule::rule => {
                 generate_pest(pair.into_inner());
             }
+            Rule::pattern => {
+                println!("pattern: {}", base_to_str(pair.into_inner()));
+            }
+            _ => {}
         }
     }
-    return result;
+    return "";
 
 }
 
@@ -26,13 +74,9 @@ fn generate_pest(pairs : Pairs<Rule>) -> &str {
 pub struct BNFParser;
 
 fn main() {
-    let thf_bnf = fs::read_to_string("src/thf.bnf").unwrap();
     let tptp_bnf = fs::read_to_string("src/tptp.bnf").unwrap();
-    let thf_parse = BNFParser::parse(Rule::file, &thf_bnf);
     let tptp_parse = BNFParser::parse(Rule::file, &tptp_bnf);
-    println!("{:?}", thf_parse.is_ok());
-    //println!("{:?}", tptp_parse.is_ok());
+    println!("{:?}", tptp_parse.is_ok());
+    generate_pest(tptp_parse.unwrap());
 
-    println!("{}", thf_parse.unwrap());
-    //println!("{}", generate_pest(thf_parse.unwrap()));
 }
