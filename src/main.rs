545ecbcaffe661084695e5fd24adbfac03e8e63b
nartan <bozec.tanguy@gmail.com>
2024-05-06 23:02:19 +0200

    made a fair bit of headway into the pest file generation for regular expressions

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,44 @@ use std::fs;
 
 fn concatable(token_rule: Rule) -> bool {
     match token_rule {
-        Rule::non_terminal | Rule::token | Rule::name => {
+        Rule::non_terminal | Rule::token | Rule::name | Rule::regex_chars 
+        | Rule::regex_expression | Rule::regex_pattern => {
             return true;
         }
         _ => {return false;}
     }
+}
+
+fn regex_to_str(pairs: Pairs<Rule>) -> String {
+    let mut res:Vec<String> = Vec::new();
+    let mut pairs = pairs.into_iter();
+    while let Some(pair) = pairs.next() {
+        let pair_rule = pair.as_rule();
+        match pair_rule {
+            Rule::regex_chars => {
+                let inner_chars = pair.into_inner();
+                if inner_chars.len() == 1 {
+                    res.push("\"".to_string() + inner_chars.as_str() + "\"");
+                }
+                else {
+                    res.push(base_to_str(inner_chars));
+                }
+                
+            }
+            Rule::regex_chars_inner => {
+            }
+            Rule::not => {
+                res.push("!".to_string());
+            }
+            Rule::dash => {
+                res.push("..".to_string());
+            }
+            _ => {
+                // fails quitely
+            }
+        }
+    }
+    return "".to_string();
 }
 
 
@@ -21,16 +54,22 @@ fn base_to_str(pairs: Pairs<Rule>) -> String {
         let pair_rule = pair.as_rule();
         match pair_rule {
             Rule::non_terminal => {
-                res.push(pair.into_inner().next().unwrap().as_str().to_string());
+                res.push(pair.clone().into_inner().next().unwrap().as_str().to_string());
             }
-            Rule::pattern => {
+            //Rule::regex_expression => {
+            //    println!("notice me!");
+            //}
+            Rule::pattern | Rule::regex_pattern | Rule::regex_expression | Rule::regex_expansion => {
                 res.push(base_to_str(pair.into_inner()));
             }
             Rule::token => {
                 res.push("\"".to_string() + pair.as_str() + "\"");
             }
-            Rule::or | Rule::star | Rule::name => {
+            Rule::or | Rule::star | Rule::name | Rule::lparen | Rule::rparen => {
                 res.push(pair.as_str().to_string());
+            }
+            Rule::regex_chars => {
+                res.push(regex_to_str(pair.into_inner()));
             }
             _ => {}
         }
@@ -46,12 +85,12 @@ fn generate_pest(pairs : Pairs<Rule>) -> &str {
         match pair.as_rule() {
             Rule::syntax_rule | Rule::regex_rule => {
                 let mut rule = pair.into_inner();
+                print!("rule: {} \n", rule.clone());
 
                 let non_terminal = rule.next().unwrap();
                 let name = base_to_str(non_terminal.into_inner());
 
                 let expansion = rule.skip(1).next().unwrap().into_inner();
-
 
                 print!("\n{} = ", name);
                 print!("{{ {} }}\n", base_to_str(expansion));
@@ -59,7 +98,7 @@ fn generate_pest(pairs : Pairs<Rule>) -> &str {
             Rule::file | Rule::rule => {
                 generate_pest(pair.into_inner());
             }
-            Rule::pattern => {
+            Rule::pattern | Rule::regex_pattern => {
                 println!("pattern: {}", base_to_str(pair.into_inner()));
             }
             _ => {}
