c1d4b8695b25b20c32be24b4b581e4933fc54c75
nartan <bozec.tanguy@gmail.com>
2024-05-20 01:24:15 +0200

    formatting and updated readme

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use pest::Parser;
 use pest::iterators::Pairs;
+use pest::Parser;
 use pest_derive::Parser;
 
 use std::fs;
 
 fn concatable(token_rule: Rule) -> bool {
     match token_rule {
-        Rule::non_terminal | Rule::token | Rule::name | Rule::regex_chars
+        Rule::non_terminal
+        | Rule::token
+        | Rule::name
+        | Rule::regex_chars
         | Rule::non_terminal_reg
-        | Rule::regex_expression | Rule::regex_pattern | Rule::regex_chars_inner
-        | Rule::class_expression | Rule::class_pattern | Rule::class_bracketed_expansion 
+        | Rule::regex_expression
+        | Rule::regex_pattern
+        | Rule::regex_chars_inner
+        | Rule::class_expression
+        | Rule::class_pattern
+        | Rule::class_bracketed_expansion
         | Rule::regex_bracketed_expansion
-        | Rule::class_expansion | Rule::regex_expansion => {
+        | Rule::class_expansion
+        | Rule::regex_expansion => {
             return true;
         }
-        _ => {return false;}
+        _ => {
+            return false;
+        }
     }
 }
 
@@ -28,16 +38,20 @@ fn escape_special_chars(string: String) -> String {
 }
 
 fn inner_regex_to_str(pairs: Pairs<Rule>) -> String {
-    let mut res:Vec<String> = Vec::new();
+    let mut res: Vec<String> = Vec::new();
     let mut pairs = pairs.into_iter();
     while let Some(pair) = pairs.next() {
         let pair_rule = pair.as_rule();
         match pair_rule {
             Rule::regex_chars_inner => {
-                res.push("\"".to_string() + &escape_special_chars(pair.as_str().to_string()) + "\"");
+                res.push(
+                    "\"".to_string() + &escape_special_chars(pair.as_str().to_string()) + "\"",
+                );
             }
             Rule::not => {
-                res.push("(!\"".to_string() + &escape_special_chars(pairs.as_str().to_string()) + "\")");
+                res.push(
+                    "(!\"".to_string() + &escape_special_chars(pairs.as_str().to_string()) + "\")",
+                );
                 return res.join(" ");
             }
             Rule::special_chars => {
@@ -48,12 +62,14 @@ fn inner_regex_to_str(pairs: Pairs<Rule>) -> String {
                 let start_char = chars_range.next().unwrap().as_str();
                 let end_char = chars_range.skip(1).next().unwrap().as_str();
                 res.push("'".to_string() + start_char + "'..'" + end_char + "'");
-                if pairs.peek().is_some_and(|r| r.as_rule() == Rule::chars_range) {
+                if pairs
+                    .peek()
+                    .is_some_and(|r| r.as_rule() == Rule::chars_range)
+                {
                     res.push("|".to_string());
                 }
             }
-            _ => {
-            }
+            _ => {}
         }
         if concatable(pair_rule) && pairs.peek().is_some_and(|r| concatable(r.as_rule())) {
             res.push("~".to_string());
@@ -62,24 +78,38 @@ fn inner_regex_to_str(pairs: Pairs<Rule>) -> String {
     return res.join(" ");
 }
 
-
 fn base_to_str(pairs: Pairs<Rule>) -> String {
-    let mut res:Vec<String> = Vec::new();
+    let mut res: Vec<String> = Vec::new();
     let mut pairs = pairs.into_iter();
     while let Some(pair) = pairs.next() {
         let pair_rule = pair.as_rule();
         match pair_rule {
             Rule::non_terminal => {
-                res.push(escape_special_chars(pair.clone().into_inner().next().unwrap().as_str().to_string()));
+                res.push(escape_special_chars(
+                    pair.clone()
+                        .into_inner()
+                        .next()
+                        .unwrap()
+                        .as_str()
+                        .to_string(),
+                ));
             }
-            Rule::pattern | Rule::class_bracketed_expansion | Rule::non_terminal_reg
+            Rule::pattern
+            | Rule::class_bracketed_expansion
+            | Rule::non_terminal_reg
             | Rule::regex_bracketed_expansion
-            | Rule::regex_pattern | Rule::regex_expression | Rule::regex_expansion
-            | Rule::class_pattern | Rule::class_expression | Rule::class_expansion => {
+            | Rule::regex_pattern
+            | Rule::regex_expression
+            | Rule::regex_expansion
+            | Rule::class_pattern
+            | Rule::class_expression
+            | Rule::class_expansion => {
                 res.push(base_to_str(pair.into_inner()));
             }
             Rule::token => {
-                res.push("\"".to_string() + &escape_special_chars(pair.as_str().to_string()) + "\"");
+                res.push(
+                    "\"".to_string() + &escape_special_chars(pair.as_str().to_string()) + "\"",
+                );
             }
             Rule::or | Rule::star | Rule::name | Rule::lparen | Rule::rparen => {
                 res.push(escape_special_chars(pair.as_str().to_string()));
@@ -92,17 +122,23 @@ fn base_to_str(pairs: Pairs<Rule>) -> String {
             }
             Rule::class_chars => {
                 let mut class_chars_inner = pair.into_inner();
-                if class_chars_inner.peek().is_some_and(|r| r.as_rule() == Rule::not) {
+                if class_chars_inner
+                    .peek()
+                    .is_some_and(|r| r.as_rule() == Rule::not)
+                {
                     res.push(inner_regex_to_str(class_chars_inner));
-                }
-                else if class_chars_inner.peek().is_some_and(|r| r.as_rule() == Rule::chars_range) {
+                } else if class_chars_inner
+                    .peek()
+                    .is_some_and(|r| r.as_rule() == Rule::chars_range)
+                {
                     res.push(inner_regex_to_str(class_chars_inner));
-                }
-                else {
+                } else {
                     res.push("(".to_string());
                     while class_chars_inner.peek().is_some() {
-                        let next_str = "\"".to_string() 
-                            + &escape_special_chars(class_chars_inner.next().unwrap().as_str().to_string())
+                        let next_str = "\"".to_string()
+                            + &escape_special_chars(
+                                class_chars_inner.next().unwrap().as_str().to_string(),
+                            )
                             + "\"";
                         res.push(next_str);
                         if class_chars_inner.peek().is_some() {
@@ -121,7 +157,7 @@ fn base_to_str(pairs: Pairs<Rule>) -> String {
     return res.join(" ");
 }
 
-fn generate_pest(pairs : Pairs<Rule>) -> &str {
+fn generate_pest(pairs: Pairs<Rule>) -> &str {
     for pair in pairs.clone() {
         match pair.as_rule() {
             Rule::syntax_rule | Rule::regex_rule | Rule::char_class_rule => {
@@ -146,7 +182,6 @@ fn generate_pest(pairs : Pairs<Rule>) -> &str {
         }
     }
     return "";
-
 }
 
 #[derive(Parser)]
@@ -168,5 +203,4 @@ fn main() {
     //let problem = fs::read_to_string("src/COM001_10.p").unwrap();
     //let tptp_problem = tptp::TPTPParser::parse(tptp::Rule::TPTP_file, &problem);
     //println!("{:?}", tptp_problem.is_ok());
-
 }
